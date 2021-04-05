70de16dd9447f38f31be027c9561e9175021531c
Mursalin7 <mursalinhq1@gmail.com>
2021-04-06 02:18:40 +0530

    fix: updated skills section

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -11,13 +11,17 @@ const Skill = () => {
           <img src='./images/icons/javascript-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
           Javascript
         </div>
-        <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='React Js'>
+        <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='React JS'>
           <img src='./images/icons/react-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
           React JS
         </div>
         <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='Springboot'>
           <img src='./images/icons/spring-boot-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
           Springboot
+        </div>
+        <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='Rest API'>
+          <img src='./images/icons/restapi-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
+          Rest API
         </div>
         <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='Redux'>
           <img src='./images/icons/redux-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
@@ -42,6 +46,18 @@ const Skill = () => {
         <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='CSS 3'>
           <img src='./images/icons/css3-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
           CSS 3
+        </div>
+        <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='SQL'>
+          <img src='./images/icons/mysql-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
+          SQL
+        </div>
+        <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='Typescript'>
+          <img src='./images/icons/typescript-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
+          Typescript
+        </div>
+        <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label='CI - CD'>
+          <img src='./images/icons/docker-original.svg' alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
+          CI - CD
         </div>
       </div>
     </section>
