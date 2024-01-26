2dc9b8ef6ba3311e29b5772a488937146de08972
Mursalin7 <mursalinhq1@gmail.com>
2024-01-27 01:12:00 +0530

    fix: updated experience

--- src/experienceData.js
+++ src/experienceData.js
@@ -1,4 +1,25 @@
  export const experience = [
+  {
+    "company": "IBM Pvt Ltd",
+    "title": "Full-Stack Developer",
+    "years": "04.2022 - present",
+    "mainTech": [
+      "ReactJS and JAVA"
+    ],
+    "technologies": [
+      "REST API",
+      "Redux",
+      "JavaScript",
+      "Springboot",
+      "Node",
+      "Docker",
+      "Kubernetes",
+      "OpenShift",
+      "Azure",
+      "Grafana",
+      "GIT"
+    ]
+  },
     {
       "company": "Tata Consultancy Services",
       "title": "Full-Stack Developer",
