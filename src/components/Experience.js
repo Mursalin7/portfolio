595d658a2c2065d40b28dfde86cb37466653b291
Mursalin7 <mursalinhq1@gmail.com>
2024-02-05 16:24:58 +0530

    fix: feb 05  updated changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -30,7 +30,7 @@ function Experience() {
     return (
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date={<div className='font-serif' style={{ backgroundColor:'black',color:'#BA8DF6',fontWeight:'bold',fontSize:'20px'}}>{work.years}</div>}
+        date={<div className='font-serif' style={{ width: 'fit-content', backgroundColor:'black', color:'#BA8DF6',fontWeight:'bold',fontSize:'20px'}}>{work.years}</div>}
         iconStyle={{
           background: "#AE944F",
           color: "#fff",
